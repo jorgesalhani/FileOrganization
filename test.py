12345364522d9f1b9128578c819945fc9ad7460d
jorgesalhani <jorge.salhani@usp.br>
2023-06-27 22:27:30 -0300

    remove stuf

--- test.py
+++
@@ -1,9 +0,0 @@
-numeros = input()
-lista = numeros.split()
-if len(lista) != 0:
-    lista = [int(i) for i in lista]
-    lista.sort()
-    lista.reverse()
-    print(lista[-1])
-else:
-    print('lista vazia')
